8f47d0d7278f3887de68e5c1922525abc66c70b9
Ody Mbegbu <odytrice@gmail.com>
2015-07-08 17:06:31 +0100

    Completed Tile Display

--- app/src/main/java/com/odytrice/popularmovies/adapters/MovieTilesAdapter.java
+++ app/src/main/java/com/odytrice/popularmovies/adapters/MovieTilesAdapter.java
@@ -1,12 +1,15 @@
 package com.odytrice.popularmovies.adapters;
 
 import android.content.Context;
+import android.media.Image;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 
+import com.odytrice.popularmovies.R;
 import com.odytrice.popularmovies.models.Movie;
+import com.odytrice.popularmovies.utils.SquaredImageView;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
@@ -23,18 +26,26 @@ public class MovieTilesAdapter extends ArrayAdapter<Movie> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
-        ImageView imageView;
+        SquaredImageView imageView;
 
-        if(convertView == null) {
+        if (convertView == null) {
             //Get Movie at Position
             if (position < 0 || position >= mMovies.size()) return null;
             Movie movie = mMovies.get(position);
 
-            imageView = new ImageView(getContext());
-            Picasso.with(getContext()).load(movie.imageUrl).into(imageView);
-        }
-        else{
-            imageView = (ImageView) convertView;
+            imageView = new SquaredImageView(getContext());
+
+            imageView.setPadding(0,0,0,0);
+
+            Picasso.with(getContext())
+                    .load(movie.imageUrl)
+                    .placeholder(R.mipmap.placeholder)
+                    .error(R.mipmap.placeholder_error)
+                    .fit()
+                    .centerCrop()
+                    .into(imageView);
+        } else {
+            imageView = (SquaredImageView) convertView;
         }
         return imageView;
     }
